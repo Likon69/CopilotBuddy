f464a5ca2c02aa27bba855a2b18ea15d6b52ba7a
Likon69 <Andrei69330@gmail.com>
2026-03-13 01:43:22 +0100

    GlobalOffsets: fix hex comments to match decimal addresses, fix ClickToMove decimal (7509504→7500800=0x727400)

--- Styx/Patchables/GlobalOffsets.cs
+++ Styx/Patchables/GlobalOffsets.cs
@@ -5,35 +5,35 @@ namespace Styx.Patchables
     public enum GlobalOffsets
     {
         // Character/Player
-        GetGUIDByKeyword = 6335472,             // 0x60A7F0
+        GetGUIDByKeyword = 6335472,             // 0x60ABF0
         ClntObjMgrGetActivePlayerObj = 4208880, // 0x4038F0
         ClntObjMgrObjectPtr = 5066160,          // 0x4D4DB0
         
         // UI/Targeting
-        CGGameUI__Target = 5393392,             // 0x524DF0
-        GetWorldState = 5541136,                // 0x548710
+        CGGameUI__Target = 5393392,             // 0x524BF0
+        GetWorldState = 5541136,                // 0x548D10
         
         // Quest
-        CGQuestInfo_C__GetAvailableQuestInfoFromIndex = 5809760, // 0x58A2E0
-        CGQuestInfo_C__GetActiveQuestFromIndex = 5810000,        // 0x58A3D0
-        CGQuestLog__IsQuestCompleted_2 = 6164656,                // 0x5E1CB0
-        CGQuestLog__GetLuaQuestIndexByID = 6155952,              // 0x5DFB30
-        CGQuestLog__AbandonSelectedQuest__ = 6163648,            // 0x5E18C0
-        CGQuestLog__IsQuestCompleted = 6164128,                  // 0x5E1AA0
-        CGQuestLog__GetQuestIdByIndex = 6174784,                 // 0x5E4340
-        CGPlayer_C__QuestLogRemoveQuest = 7163776,               // 0x6D4380
+        CGQuestInfo_C__GetAvailableQuestInfoFromIndex = 5809760, // 0x58A660
+        CGQuestInfo_C__GetActiveQuestFromIndex = 5810000,        // 0x58A750
+        CGQuestLog__IsQuestCompleted_2 = 6164656,                // 0x5E10B0
+        CGQuestLog__GetLuaQuestIndexByID = 6155952,              // 0x5DEEB0
+        CGQuestLog__AbandonSelectedQuest__ = 6163648,            // 0x5E0CC0
+        CGQuestLog__IsQuestCompleted = 6164128,                  // 0x5E0EA0
+        CGQuestLog__GetQuestIdByIndex = 6174784,                 // 0x5E3840
+        CGPlayer_C__QuestLogRemoveQuest = 7163776,               // 0x6D4F80
         
         // Input
-        CGInputControl__UpdatePlayer = 6273984, // 0x5FB3C0
-        CGInputControl__ToggleControlBit = 6274576, // 0x5FB610
+        CGInputControl__UpdatePlayer = 6273984, // 0x5FBBC0
+        CGInputControl__ToggleControlBit = 6274576, // 0x5FBE10
         
         // Items
-        CGItem_C__Use = 7375904,                // 0x709020
-        CGItem_C__CreateItemLink = 6414992,     // 0x61E690
-        CGItem_C__CreateItemLink2 = 6415264,    // 0x61E7A0
-        CGItem_C__BuildItemName = 7368048,      // 0x707170
-        CGItemStats_C = 6424480,                // 0x620BA0
-        CGPlayer_C__CanUseItem = 7193584,       // 0x6DB5F0
+        CGItem_C__Use = 7375904,                // 0x708C20
+        CGItem_C__CreateItemLink = 6414992,     // 0x61E290
+        CGItem_C__CreateItemLink2 = 6415264,    // 0x61E3A0
+        CGItem_C__BuildItemName = 7368048,      // 0x706D70
+        CGItemStats_C = 6424480,                // 0x6207A0
+        CGPlayer_C__CanUseItem = 7193584,       // 0x6DC3F0
         
         // Cache - Creature
         DbCreatureCache_GetInfoBlockById = 6796960, // 0x67B6A0
@@ -72,7 +72,7 @@ namespace Styx.Patchables
         DbItemTextCache_GetInfoBlockById = 6812864, // 0x67F4C0
         
         // Cache - WoW
-        DbWoWCache_GetInfoBlockById = 6814336,      // 0x67FAA0
+        DbWoWCache_GetInfoBlockById = 6814336,      // 0x67FA80
         
         // Cache - PageText
         DbPageTextCache_GetInfoBlockById = 6816112, // 0x680170
@@ -81,31 +81,31 @@ namespace Styx.Patchables
         DbDanceCache_GetInfoBlockById = 6817488,    // 0x6806D0
         
         // Client Database
-        ClientDb_RegisterBase = 6502352,            // 0x633DD0
+        ClientDb_RegisterBase = 6502352,            // 0x6337D0
         
         // Doors
-        CGDoor_C__CanOpenNow = 7412176,             // 0x712AD0
+        CGDoor_C__CanOpenNow = 7412176,             // 0x7119D0
         
         // Environment
-        IsOutdoors = 7452656,                       // 0x71C7F0
+        IsOutdoors = 7452656,                       // 0x71B7F0
         
         // Localization
-        FrameScript__GetLocalizedText = 7480800,    // 0x7233E0
+        FrameScript__GetLocalizedText = 7480800,    // 0x7225E0
         
         // Units
-        CGUnit_C__UnitReaction = 7492032,           // 0x725FC0
-        CGUnit_C_CalculateThreat = 7566528,         // 0x737AC0
+        CGUnit_C__UnitReaction = 7492032,           // 0x7251C0
+        CGUnit_C_CalculateThreat = 7566528,         // 0x7374C0
         
 // Movement - FROM 335offsetsall.txt
-		CGPlayer_C__ClickToMove = 7509504,          // 0x727400
+		CGPlayer_C__ClickToMove = 7500800,          // 0x727400
 		CGPlayer_C__ClickToMoveStop = 7517088,      // 0x72B3A0
         
         // Collision
-        TraceLine = 8010608,                        // 0x7A3770
+        TraceLine = 8010608,                        // 0x7A3B70
         
         // Spells
-        Spell_C__GetSpellCooldown = 8419712,        // 0x806A80
-        Spell_C__HandleTerrainClick = 8438592,      // 0x80B340
+        Spell_C__GetSpellCooldown = 8419712,        // 0x807980
+        Spell_C__HandleTerrainClick = 8438592,      // 0x80C340
         Spell_C__CastSpell = 8444480,               // 0x80DA40
         
         // Lua
@@ -118,6 +118,6 @@ namespace Styx.Patchables
         FrameScript_Load = 8714336,                 // 0x84F860
         
         // Performance
-        PerformanceCounter = 8826400                // 0x86C220
+        PerformanceCounter = 8826400                // 0x86AE20
     }
 }
